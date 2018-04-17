8f57fad437bed93d60d3e9001b7f452840a77d84
maikelSoFly <mikolaj.stepniewski1@gmail.com>
2018-04-18 00:04:22 +0200

    Added states and components examples

--- mstepniewski/ie-component-lecture/src/App.js
+++ mstepniewski/ie-component-lecture/src/App.js
@@ -1,21 +1,98 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Greeting from './components/Greeting'
+import List from './components/List'
+import WillItBoil from './components/WillItBoil'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+   state = {
+      unleashed: false,
+      childrenList: [],
+      buttonText: 'SHOW ME MORE',
+      message: '',
+      scale: 'C',
+      temperature: 0,
+   }
+
+   unleash = () => {
+      this.setState({
+         unleashed: !this.state.unleashed,
+         buttonText: this.state.unleashed ? 'SHOW ME MORE' : 'SHOW ME LESS'
+      })
+   }
+
+   componentWillMount = () => {
+      console.log('▶️ App component will mount!')
+   }
+
+   componentDidMount() {
+      const list = [
+         <div>
+            <input
+               type="radio"
+               name="scale"
+               value="C"
+               defaultChecked={'C' === this.state.scale}
+               onChange={this.onRadioChanged} /> Celsius
+         </div>,
+
+         <div>
+            <input
+               type="radio"
+               name="scale"
+               value="F"
+               defaultChecked={'F' === this.state.scale}
+               onChange={this.onRadioChanged} /> Fahrenheit
+         </div>,
+         <input placeholder='insert value...' onChange={this.onInputChange} />
+      ]
+
+      this.setState({ childrenList: list })
+   }
+
+   onInputChange = event => {
+      const input = event.target
+      this.setState({ temperature: input.value }, () => {
+         // console.log(this.state.temperature)
+      })
+   }
+
+   onRadioChanged = event => {
+      const radio = event.target
+      this.setState({ scale: radio.value }, () => {
+         console.log('Changed to ' + this.state.scale)
+      })
+   }
+
+
+   render() {
+      return (
+         <div className="App">
+            <Greeting />
+
+            {this.state.unleashed &&
+               <WillItBoil temperature={this.state.temperature} scale={this.state.scale} />
+            }
+
+            <button
+               type='button'
+               style={{ marginTop: '30px' }}
+               onClick={this.unleash}
+            >
+               {this.state.buttonText}
+            </button>
+
+            {this.state.unleashed &&
+               <List
+                  children={this.state.childrenList}
+                  temperature={this.state.temperature}
+                  scale={this.state.scale}
+               />
+            }
+
+         </div >
+      );
+   }
 }
 
 export default App;
---
+++ mstepniewski/ie-component-lecture/src/components/WillItBoil.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import elmo from '../elmo.gif'
+
+const checkBoiling = (temperature, scale) => {
+   if (scale === 'C') {
+      return temperature >= 100 ? true : false
+   }
+   else if (scale === 'F') {
+      return temperature >= 212 ? true : false
+   }
+}
+
+class WillItBoil extends Component {
+   state = {
+      willBoil: false,
+      temperature: null,
+      scale: null
+   }
+
+   static getDerivedStateFromProps = (props, currentState) => {
+      if (currentState.temperature !== props.temperature || currentState.scale !== props.scale) {
+         return {
+            temperature: props.temperature,
+            scale: props.scale,
+            willBoil: checkBoiling(props.temperature, props.scale)
+         }
+      }
+
+      return null
+   }
+
+   componentWillUnmount = () => {
+      console.log('⏏️ WillItBoil component will be unmounted!')
+   }
+
+
+   render() {
+      const style = {
+         backgroundColor: this.state.willBoil ? '#000' : 'lightgray',
+         color: this.state.willBoil ? '#fff' : '#000',
+         fontSize: this.state.willBoil ? '30px' : '20px',
+         display: 'flex',
+         justifyContent: 'center',
+         alignContent: 'center',
+         flexDirection: 'column',
+         height: '80px',
+         marginTop: '30px',
+         backgroundImage: this.state.willBoil ? `url(${elmo})` : null,
+         backgroundRepeat: 'repeat-y',
+         backgroundPosition: 'center',
+      }
+
+      return (
+         <div style={style}>
+            {this.state.willBoil ? '🔥 IT WILL BOIL 🔥' : 'It will NOT boil 💧'}
+         </div>
+      )
+   }
+}
+
+export default WillItBoil
