cd894a550394966afe31cb7c588ecee6e392f168
Radoslaw Pieja <pieja.radoslaw@gmail.com>
2018-04-17 23:41:19 +0200

    Add console logging example

--- rpieja/sample-app/src/App.js
+++ rpieja/sample-app/src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 
 class App extends Component {
   render() {
+    function sthFunny() {
+      console.log("not funny")
+    }
+
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +17,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={sthFunny}>C'mon, do something funny</button>
       </div>
     );
   }
