2753c118236d66a4df7fe5d2465349f4f4a56cbd
Tomasz Zapiórkowski <zapiorkowski@gmail.com>
2018-04-17 21:36:51 +0200

    forms ok, validation nope

---
+++ tzapiorkowski/Forms/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import FormExample from './FormExample.js'
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <FormExample />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ tzapiorkowski/Forms/src/FormExample.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {ControlLabel, FormControl, FormGroup, HelpBlock} from "react-bootstrap";
+
+class FormExample extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            value: ''
+        };
+    }
+
+    getValidationState = () => {
+        const length = this.state.value.length;
+        if (length > 10) return 'success';
+        else if (length > 5) return 'warning';
+        else if (length > 0) return 'error';
+        return null;
+    };
+
+    handleChange = e => {
+        this.setState({ value: e.target.value });
+    };
+
+    render() {
+        return (
+            <form>
+                <FormGroup
+                    controlId="formBasicText"
+                    validationState={this.getValidationState}
+                >
+                    <ControlLabel>Working example with validation</ControlLabel>
+                    <FormControl
+                        type="text"
+                        value={this.state.value}
+                        placeholder="Enter text"
+                        onChange={this.handleChange}
+                    />
+                    <FormControl.Feedback />
+                    <HelpBlock>Validation is based on string length.</HelpBlock>
+                </FormGroup>
+            </form>
+        );
+    }
+}
+export default FormExample;
